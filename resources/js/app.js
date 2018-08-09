28831b926573fa387b002f3758c6d8f6db50b60f
abolzz <abolzzy@gmail.com>
2018-08-09 12:36:16 +0300

    slider

--- resources/js/app.js
+++ resources/js/app.js
@@ -243,4 +243,26 @@ $(document).ready(function() {
 
   $(".site-header").css({'height': "75px" + $('.header-menu').height()});
 
+  // Slider
+  var slideIndex = 0;
+  showSlides();
+
+  function showSlides() {
+      let i,
+      slides = document.getElementsByClassName("slider-item"),
+      dots = document.getElementsByClassName("dot");
+      for (i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none"; 
+      }
+      if (slideIndex > slides.length) {slideIndex = 1}    
+      for (i = 0; i < dots.length; i++) {
+          dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slideIndex++;
+      if (slideIndex > slides.length) {slideIndex = 1}
+      dots[slideIndex-1].className += " active";
+      slides[slideIndex-1].style.display = "block";
+      setTimeout(showSlides, 2000); // Change image every 2 seconds
+  } 
+
 });
