129c47d9df1feb810792e27512555df88af1af4c
abolzz <abolzzy@gmail.com>
2018-08-10 18:11:00 +0300

    table

--- resources/js/app.js
+++ resources/js/app.js
@@ -305,6 +305,6 @@ $(document).ready(function() {
       dots[slideIndex-1].className += " active";
       slides[slideIndex-1].style.display = "block";
       setTimeout(showSlides, 2000); // Change image every 2 seconds
-  } 
+  }
 
 });
